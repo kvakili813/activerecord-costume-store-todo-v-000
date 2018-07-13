5784e59fb1bf0d46f822f145d00cbd276f0d3773
Kayvan Vakili <kvakili813@gmail.com>
2018-07-13 15:52:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStore < ActiveRecord::Migration[5.0]
+class CreateCostumeStores < ActiveRecord::Migration[5.0]
   def change
     create_table :costume_stores do |t|
       t.string :name
